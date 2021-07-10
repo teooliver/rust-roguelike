80320f97f972b9177d468351dd3f03b09a6083eb
Teo Oliver <teo.liz.oli@gmail.com>
2021-07-10 23:59:23 +0100

    add: entities to generic render system

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,13 @@ impl GameState for State {
         //we are telling out context that it should clear the virtual terminal.
         ctx.cls();
         // print "Hello Rust World" at the location (1,1).
-        ctx.print(1, 1, "Hello Rust Wold");
+        // ctx.print(1, 1, "Hello Rust Wold");
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+
+        for (pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
     }
 }
 
@@ -51,5 +57,16 @@ fn main() -> rltk::BError {
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
 
+    for i in 0..10 {
+        gs.ecs
+            .create_entity()
+            .with(Position { x: i * 7, y: 20 })
+            .with(Renderable {
+                glyph: rltk::to_cp437('☺'),
+                fg: RGB::named(rltk::RED),
+                bg: RGB::named(rltk::BLACK),
+            })
+            .build();
+    }
     rltk::main_loop(context, gs)
 }
